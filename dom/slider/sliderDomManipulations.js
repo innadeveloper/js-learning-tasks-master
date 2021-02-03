8d75f64e3ad94b22d7d7b1f1af0713873f2eafa6
innadeveloper <74996544+innadeveloper@users.noreply.github.com>
2021-02-03 22:29:42 +0100

    Add files via upload

---
+++ dom/slider/sliderDomManipulations.js
@@ -0,0 +1,21 @@
+const leftBtn = document.querySelector("#left");
+const rightBtn = document.querySelector("#right");
+const itemsList = document.querySelector("#items");
+
+const loop = (direction, e) => {
+  e.preventDefault();
+
+  if (direction == "right") {
+    itemsList.appendChild(itemsList.firstElementChild);
+  } else {
+    itemsList.insertBefore(itemsList.lastElementChild, items.firstElementChild);
+  }
+};
+
+right.addEventListener("click", e => {
+  loop("right", e);
+});
+
+left.addEventListener("click", e => {
+  loop("left", e);
+});
